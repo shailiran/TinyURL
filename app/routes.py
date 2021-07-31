9614f5c56c96adfeab7a68914330bdcc1b1754f0
shailiran <shailiran@gmail.com>
2021-07-31 17:12:17 +0300

    fix indentations and delete unused import

--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,36 @@
 from flask import Blueprint, redirect, url_for, render_template
 from flask.globals import request
-
 from .extensions import db
 from .models import URL
 
 short = Blueprint('short', __name__)
-counter = 6
+counter = 10
 
 @short.route('/', methods=['POST', 'GET'])
 def home():
-  global counter
-  if request.method == 'POST':
-    url_received = request.form['nm']
-    found_url = URL.query.filter_by(original_url=url_received).first()
-    if found_url:
-      return redirect(url_for('short.display_short_url', url=found_url.short_url))
+    global counter
+    if request.method == 'POST':
+        url_received = request.form['nm']
+        found_url = URL.query.filter_by(original_url=url_received).first()
+        if found_url:
+            return redirect(url_for('short.display_short_url', url=found_url.short_url))
+        else:
+            new_url = URL(url_received, counter)
+            counter += 1
+            db.session.add(new_url)
+            db.session.commit()
+            short_url = new_url.short_url
+            return redirect(url_for('short.display_short_url', url=short_url)) # TODO - change - this is not working!
     else:
-      new_url = URL(url_received, counter)
-      counter += 1
-      db.session.add(new_url)
-      db.session.commit()
-      short_url = new_url.short_url
-      return redirect(url_for('short.display_short_url', url=short_url)) # TODO - change - this is not working!
-  else:
-    return render_template('url_page.html')
+        return render_template('url_page.html')
 
 @short.route('/<short_url>')
 def redirect_to_url(short_url):
-  original_url = URL.query.filter_by(short_url=short_url).first()
-  if original_url:
-    return redirect(original_url.original_url)
-  else:
-    return 'URL does not exist'
+    original_url = URL.query.filter_by(short_url=short_url).first()
+    if original_url:
+        return redirect(original_url.original_url)
+    else:
+        return 'URL does not exist'
 
 @short.route('/display/<url>')
 def display_short_url(url):
