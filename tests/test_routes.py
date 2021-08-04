4dec87953eb43daa3468397af3829a32d32ec858
shailiran <shailiran@gmail.com>
2021-08-04 10:52:27 +0300

    done

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,7 +2,20 @@ def test_base_route_status(client):
     rv = client.get('/')
     assert rv.status_code == 200
 
-def test_add_url(client, mock_url):
-    response = client.post('/', data=mock_url.short_url)
-    assert(302, response.status_code)
-    assert(mock_url.original_url, response.location)
+def test_post_method(client, mock_url):
+    short_url = mock_url.short_url
+    response = client.post('/', data=short_url)
+    assert 302, response.status_code
+    assert mock_url.original_url, response.location
+
+def test_short_url_route(client, mock_url):
+    short_url = mock_url.short_url
+    response = client.get('/<short_url>')
+    assert 302, response.status_code
+    assert mock_url.original_url, response.location
+
+def test_display_route(client, mock_url):
+    short_url = mock_url.short_url
+    response = client.get('/display/<short_url>')
+    assert 302, response.status_code
+    assert mock_url.original_url, response.location
