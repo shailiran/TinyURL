e4dd333988f418977d1dee8bd0e100318ec20e24
shailiran <shailiran@gmail.com>
2021-08-05 20:23:01 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,9 @@
 import pytest
 import os
 import tempfile
-from uuid import uuid4
-from flask_sqlalchemy import SQLAlchemy
 
 from app import create_app
 from app.extensions import db
-from app.models import URL
 
 @pytest.fixture
 def client():
@@ -22,26 +19,11 @@ def client():
         with app.app_context():
             db.create_all()
             db.session.commit()
+
         yield client
+
         db.session.remove()
         db.drop_all()
             
     os.close(db_fd)
     os.unlink(db_path)
-
-@pytest.fixture
-def mock_url_1():
-    return URL(original='original_1', counter=1)
-
-@pytest.fixture
-def mock_url_2():
-    return URL(original='original_2', counter=2)
- 
-# @pytest.fixture
-# def mock_get_sqlalchemy(mocker):
-#     mock = mocker.patch('flask_sqlalchemy._QueryProperty.__get__').return_value = mocker.Mock()
-#     return mock
-
-# @pytest.fixture
-# def mock_encoder(mocker):
-#     return mocker.patch("app.utils.base62_encoder")
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,16 +1,24 @@
+from app.models import URL
 import pytest
-import logging
-import sys
 
 def test_base_route_status(client):
     rv = client.get('/')
     assert rv.status_code == 200
 
 @pytest.mark.parametrize('original_url', ('original_1', 'original_2'))
-def test_post_method(client, original_url):
+def test_post_method_not_found(client, original_url):
     response = client.post('/', data={'nm': original_url})
-    logger = logging.getLogger()
-    logger.error(response.location)
+    assert response.status_code == 302
+
+@pytest.mark.parametrize('original_url', ('original_1', 'original_2'))
+def test_post_method_found(client, original_url):
+    response = client.post('/', data={'nm': original_url})
+    response = client.post('/', data={'nm': original_url})
+    assert response.status_code == 302
+
+@pytest.mark.parametrize('original_url', ('original_3', 'original_4'))
+def test_post_method_not_found(client, original_url):
+    response = client.post('/', data={'nm': original_url})
     assert response.status_code == 302
 
 @pytest.mark.parametrize('path', ('/display/000001', '/display/000002'))
@@ -18,19 +26,15 @@ def test_display_route(client, path):
     response = client.get(path)
     assert response.status_code == 200
 
-@pytest.mark.parametrize('path', ('/000001', '/000002'))
-def test_short_url_exist_route(client, path):
-    response = client.get(path)
-    location = 'http://localhost/original_' + path[-1]
-    assert response.status_code == 302
-    assert response.location == location
+def test_short_url_exist_route(client):
+    post_response = client.post('/', data={'nm': 'original_1'})
+    assert post_response.status_code == 302
+    url = URL.query.filter_by(original_url='original_1').first()
+    get_response = client.get(url.short_url)
+    assert get_response.status_code == 302
 
 @pytest.mark.parametrize('path', ('/12', '/00002'))
 def test_short_url_not_exist_route(client, path):
     response = client.get(path)
     assert 'URL does not exist' in response.data
 
-
-    # logger = logging.getLogger()
-    # logger.error(response.data)
-    # logger.error(response.location)
